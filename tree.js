ec85f74684509e075f0dcf8402ca290321f12c5c
zhengzf <369194105@qq.com>
2016-06-28 11:00:15 +0800

    bug01 resolve

--- tree.js
+++ tree.js
@@ -1,6 +1,12 @@
 var treeModule=(function(argument) {
 	var tree={
-
+		init:function(json) {
+			var ele=document.querySelector(".tree");
+			ele.appendChild("<span></span>");
+			for (var i = 0; i < json.length; i++) {
+				console.log(json[i]);
+			}
+		}
 	};
 
 	return tree;
